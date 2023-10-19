9e021f38b528a987678768c9707693b548ac4003
Noel Gonzalez Ornelas <noel.gonzalezornelas@mercadolibre.com.mx>
2023-10-19 00:27:12 -0600

    Siguiente commit

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ngonzalezo/godesdecero/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
