d7ad555b8c3a20ed2327136450b583d028d16752
Noel Gonzalez Ornelas <noel.gonzalezornelas@mercadolibre.com.mx>
2023-10-18 23:12:42 -0600

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ngonzalezo/godesdecero
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ngonzalezo/godesdecero/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
